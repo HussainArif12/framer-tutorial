20a2288cf5013938cf43aac8b48332597d96a921
HussainArif <hussainarifkl@gmail.com>
2021-01-20 22:32:08 +0500

    Router build

--- src/About.js
+++ src/About.js
@@ -1,9 +1,11 @@
+import { motion } from "framer-motion";
+
 function About() {
   return (
-    <>
+    <motion.div initial={{ x: -100 }} animate={{ x: 0 }}>
       <h1>Simple About page</h1>
       <p>Built with React!</p>
-    </>
+    </motion.div>
   );
 }
 export default About;
--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
-import { motion } from "framer-motion";
+import { motion, useCycle } from "framer-motion";
 import { Link } from "react-router-dom";
 function Home() {
+  const [x, setX] = useCycle(10, 20, 40);
   const variants = {
     initial: {
       opacity: 0,
@@ -14,26 +15,35 @@ function Home() {
       opacity: 0,
       x: -100,
     },
+    pStyle: {
+      x: x,
+    },
   };
   return (
-    <motion.div
-      variants={variants}
-      initial="initial"
-      animate="animate"
-      exit="exit"
-      transition={{ duration: 1 }}
-    >
-      <h1>Welcome home!</h1>
-      <h1>Type of coffees:</h1>
-      <ul>
-        <li>
-          <Link to={"/coffee/hot"}>Hot Coffees</Link>
-        </li>
-        <li>
-          <Link to="/coffee/iced">Cold coffees</Link>
-        </li>
-      </ul>
-    </motion.div>
+    <>
+      <motion.div
+        variants={variants}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        transition={{ duration: 1 }}
+      >
+        <h1>Welcome home!</h1>
+        <h1>Type of coffees:</h1>
+        <ul>
+          <li>
+            <Link to={"/coffee/hot"}>Hot Coffees</Link>
+          </li>
+          <li>
+            <Link to="/coffee/iced">Cold coffees</Link>
+          </li>
+        </ul>
+      </motion.div>
+      <motion.p variants={variants} animate="pStyle">
+        Hello World
+      </motion.p>
+      <button onClick={() => setX()}>button</button>
+    </>
   );
 }
 export default Home;
