a86ef979c92c4bd882598be067bb2e473aad6c9b
HussainArif <hussainarifkl@gmail.com>
2021-01-20 17:40:50 +0500

    Working build stable

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
 function App() {
+  const [disappear, setDisappear] = useState(false);
+  const variants = {
+    deleted: {
+      opacity: 0.5,
+    },
+    visible: {
+      opacity: 1,
+    },
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <motion.h1
+        initial={{ y: 250 }}
+        animate={{ y: 10 }}
+        whileHover={{ scale: 10 }}
+        transition={{ duration: 0.5 }}
+      >
+        <h1> Hello world </h1>
+      </motion.h1>
+      <motion.div whileHover="deleted" variants={variants}>
+        <p>Hello</p>
+      </motion.div>
+      <AnimatePresence>
+        {!disappear && (
+          <motion.p
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            This is animate presence!
+          </motion.p>
+        )}
+      </AnimatePresence>
+      <motion.button
+        whileHover={{ scaleX: 2, scaleY: 2 }}
+        onClick={() => setDisappear((prevState) => !prevState)}
+      >
+        Appear/Disappear
+      </motion.button>
     </div>
   );
 }
